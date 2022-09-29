6ad37debf0a6b8a78425c0752068115643d251ab
Seneen Khan <93351933+seneenkhan@users.noreply.github.com>
2022-09-29 15:22:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+const items = ["Buy Food","Cook Food","Eat Food"];
+const workItems =[];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded ({
+  extended: true
+}));
+
+
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+  res.render("list", {
+    listTitle: day, newlistItems: items
+  });
+});
+
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+
+  }
+  else{
+  items.push(item);
+  res.redirect("/");
+}
+
+});
+app.get("/work", function(req,res){
+  res.render("list",{
+    listTitle: " Work List" , newlistItems: workItems });
+});
+
+  app.post("/work", function(req,res){
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+  });
+app.listen(3000, function() {
+  console.log("Server started at port 3000");
+});
